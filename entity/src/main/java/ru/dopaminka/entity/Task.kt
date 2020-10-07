5e2d61c3670cc9dd9c912b21d3ef6856a0c11b51
Viktor Statkevich <vikvikst474@gmail.com>
2020-10-07 08:32:20 +0700

    fun removeIllustration added in class Task.kt

--- entity/src/main/java/ru/dopaminka/entity/Task.kt
+++ entity/src/main/java/ru/dopaminka/entity/Task.kt
@@ -2,7 +2,11 @@ package ru.dopaminka.entity
 
 abstract class Task
 var completed : Boolean = false
-
+val illustrations: Illustration
+    get() {
+        TODO()
+    }
 
 fun complete() {}
 fun addIllustration(){}
+fun removeillustrtion(){}
