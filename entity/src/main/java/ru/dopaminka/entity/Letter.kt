aa3a1ec0d48e0c2aed71f61c475b463a61e1a9a8
Viktor Statkevich <vikvikst474@gmail.com>
2020-10-07 08:23:10 +0700

    values "sound" and "text" added in class Letter.kt

--- entity/src/main/java/ru/dopaminka/entity/Letter.kt
+++ entity/src/main/java/ru/dopaminka/entity/Letter.kt
@@ -1,8 +1,11 @@
 package ru.dopaminka.entity
 
 import sun.misc.Resource
+import kotlin.properties.Delegates
 
 class Letter {
+    var sound by Delegates.notNull<Int>()
+    lateinit var text: String
     fun setSound() {
 
     }
