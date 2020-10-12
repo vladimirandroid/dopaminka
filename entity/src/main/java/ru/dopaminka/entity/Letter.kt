d90db9fa21e7d8ba151489f75f09ba7a4ab01d4e
Vladimir Ryashentsev <vladimir.r@goodvas.com>
2020-10-12 19:58:32 +0700

    Clean

--- entity/src/main/java/ru/dopaminka/entity/Letter.kt
+++ entity/src/main/java/ru/dopaminka/entity/Letter.kt
@@ -1,12 +1,3 @@
 package ru.dopaminka.entity
 
-import sun.misc.Resource
-import kotlin.properties.Delegates
-
-class Letter(_text: String, _sound: Int) {
-    var text = _text
-    var sound = _sound
-    fun setSound() {
-
-    }
-}
+class Letter(var text: String, var soundFileName: String)
